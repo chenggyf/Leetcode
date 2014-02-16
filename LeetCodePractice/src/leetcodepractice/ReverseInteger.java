2f84822728cb8b8b6741dcf89c181adee8ff5542
Cheng Guo <wonderful.guo@gmail.com>
2014-02-15 20:31:52 -0500

    Add Leetcode

---
+++ LeetCodePractice/src/leetcodepractice/ReverseInteger.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leetcodepractice;
+
+/**
+ *
+ * @author Cheng Guo
+ */
+public class ReverseInteger {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static int reverse(int x) {
+        String xx = Integer.toString(x);
+        String reverse;
+        if (x == 0) {
+            reverse = "0";
+        } else {
+            if (x > 0) {
+                reverse = new StringBuffer(xx).reverse().toString();
+            } else {
+                reverse = "-"  + new StringBuffer(xx.substring(1)).reverse().toString();
+            }
+        }
+        return Integer.parseInt(reverse);
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(reverse(1000000003));
+    }
+
+}
